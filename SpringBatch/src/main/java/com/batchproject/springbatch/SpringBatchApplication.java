ea75ae693c14bac8e33785e26c170707212582b9
amazingkj <jiin724@gmail.com>
2023-01-17 18:00:01 +0900

    EnableTask 어노테이션, 의존성 추가

--- SpringBatch/src/main/java/com/batchproject/springbatch/SpringBatchApplication.java
+++ SpringBatch/src/main/java/com/batchproject/springbatch/SpringBatchApplication.java
@@ -4,7 +4,9 @@ package com.batchproject.springbatch;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
 
+@EnableTask
 @EnableBatchProcessing
 @SpringBootApplication
 public class SpringBatchApplication {
